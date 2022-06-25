38ad4475abee9d2c69ae3762b6ed9e06a9ce68db
Sadirov Izzatillo <sadirovizzatillo3@gmail.com>
2022-06-25 20:54:00 +0500

    commit

---
+++ book/components/Search.vue
@@ -0,0 +1,14 @@
+<template>
+  <form class="search-form">
+    <input class="search-form__input" type="search" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'SearchBook',
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ book/pages/Books.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1 v-if="all">{{ all }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AllBooks',
+
+        async asyncData({ $axios }) {
+            try {
+                const { data } = await $axios.get('volumes/', {
+                    params: { q: 'All'}
+                })
+                return {
+                    all: data
+                }
+            }
+            catch(err){
+                console.log(err)
+            }
+        }
+
+    }
+</script>
